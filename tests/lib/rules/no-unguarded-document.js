9715f95f0bfc5e1e00e6281aae00570407421bcb
Steve Calvert <steve.calvert@gmail.com>
2016-12-05 15:37:43 -0800

    Adding rule for no-unguarded-document + tests (#15)
    * Adding rule for no-unguarded-document + tests

---
+++ tests/lib/rules/no-unguarded-document.js
@@ -0,0 +1,73 @@
+const rule = require('../../../lib/rules/no-unguarded-document');
+const MESSAGE = rule.meta.message;
+const RuleTester = require('eslint').RuleTester;
+const ruleTester = new RuleTester();
+
+ruleTester.run('no-timers', rule, {
+  valid: [
+    {
+      code: `
+        export default Ember.Component.extend({
+          actions: {
+            foo() {
+              if (environment.isBrowser()) {
+                const node = document.querySelector('blah');
+              }
+            }
+          }
+        });`,
+      parserOptions: {
+        ecmaVersion: 6,
+        sourceType: 'module'
+      }
+    },
+    {
+      code: `
+        export default Ember.Component.extend({
+          guarded() {
+            if (environment.isBrowser()) {
+                const node = document.querySelector('blah');
+              }
+          }
+        });`,
+      parserOptions: {
+        ecmaVersion: 6,
+        sourceType: 'module'
+      }
+    }
+  ],
+  invalid: [
+    {
+      code: `
+        export default Ember.Component({
+          actions: {
+            bar() {
+              const node = document.querySelector('blah');
+            }
+          }
+        });`,
+      parserOptions: {
+        ecmaVersion: 6,
+        sourceType: 'module'
+      },
+      errors: [{
+        message: MESSAGE
+      }]
+    },
+    {
+      code: `
+        export default Ember.Component({
+          baz() {
+            const node = document.querySelector('blah');
+          }
+        });`,
+      parserOptions: {
+        ecmaVersion: 6,
+        sourceType: 'module'
+      },
+      errors: [{
+        message: MESSAGE
+      }]
+    }
+  ]
+});
