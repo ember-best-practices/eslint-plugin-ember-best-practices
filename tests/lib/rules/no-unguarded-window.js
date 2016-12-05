64b2b6149bafc742f4bd8d6b1c83b2476045c006
Steve Calvert <scalvert@linkedin.com>
2016-12-05 15:34:15 -0800

    Adding rule for no-unguarded-window

---
+++ tests/lib/rules/no-unguarded-window.js
@@ -0,0 +1,73 @@
+const rule = require('../../../lib/rules/no-unguarded-window');
+const MESSAGE = rule.meta.message;
+const RuleTester = require('eslint').RuleTester;
+const ruleTester = new RuleTester();
+
+ruleTester.run('no-unguarded-window', rule, {
+  valid: [
+    {
+      code: `
+        export default Ember.Component.extend({
+          actions: {
+            guarded() {
+              if (environment.isBrowser()) {
+                const location = window.location;
+              }
+            }
+          }
+        });`,
+      parserOptions: {
+        ecmaVersion: 6,
+        sourceType: 'module'
+      }
+    },
+    {
+      code: `
+        export default Ember.Component.extend({
+          guarded() {
+            if (environment.isBrowser()) {
+              const location = window.location;
+            }
+          }
+        });`,
+      parserOptions: {
+        ecmaVersion: 6,
+        sourceType: 'module'
+      }
+    }
+  ],
+  invalid: [
+    {
+      code: `
+        export default Ember.Component({
+          actions: {
+            unguarded() {
+              const location = window.location;
+            }
+          }
+        });`,
+      parserOptions: {
+        ecmaVersion: 6,
+        sourceType: 'module'
+      },
+      errors: [{
+        message: MESSAGE
+      }]
+    },
+    {
+      code: `
+        export default Ember.Component({
+          unguarded() {
+            const location = window.location;
+          }
+        });`,
+      parserOptions: {
+        ecmaVersion: 6,
+        sourceType: 'module'
+      },
+      errors: [{
+        message: MESSAGE
+      }]
+    }
+  ]
+});
