a343d29a8173d15cf15871b04f3a19e0e08abe92
Marc Lynch <lynchbomb@users.noreply.github.com>
2017-06-21 17:26:47 -0700

    Add rule against using specific deprecated jQuery Methods (Blacklisted) (#59)
    * Deprecate jQuery N-Method

---
+++ guides/rules/no-jquery-methods.md
@@ -0,0 +1,6 @@
+# No jQuery Methods
+
+**TL;DR** The intent of this rule is to identify any blacklisted jQuery methods as deprecated.
+
+Provides the ability to blacklist a set of jQuery methods for deprecation. 
+The eventual goal being a complete removal of jQuery.
---
+++ tests/lib/rules/no-jquery-methods.js
@@ -0,0 +1,135 @@
+const rule = require('../../../lib/rules/no-jquery-methods');
+const RuleTester = require('eslint').RuleTester;
+const getMessage = rule.meta.message;
+const BLACKLISTMETHOD = 'add';
+const ruleTester = new RuleTester({
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module'
+  }
+});
+
+ruleTester.run('no-jquery-methods', rule, {
+  valid: [
+    {
+      code: `
+        export default Ember.Component({
+          init() {
+            Ember.$().notBlacklistedMethod();
+          }
+        });`,
+      options: [BLACKLISTMETHOD]
+    },
+    {
+      code: `
+        export default Ember.Component({
+          init() {
+            this.$().notBlacklistedMethod();
+          }
+        });`,
+      options: [BLACKLISTMETHOD]
+    },
+    {
+      code: `
+        export default Ember.Component({
+          init() {
+            const myObj = {};
+            myObj[${BLACKLISTMETHOD}]();      
+          }
+        });`,
+      options: [BLACKLISTMETHOD]
+    }
+  ],
+  invalid: [
+    {
+      code: `
+        export default Ember.Component({
+          init() {
+            Ember.$()[${BLACKLISTMETHOD}]();
+          }
+        });`,
+      options: [BLACKLISTMETHOD],
+      errors: [{
+        message: getMessage(BLACKLISTMETHOD)
+      }]
+    },
+    {
+      code: `
+        export default Ember.Component({
+          init() {
+            this.$()[${BLACKLISTMETHOD}]();
+          }
+        });`,
+      options: [BLACKLISTMETHOD],
+      errors: [{
+        message: getMessage(BLACKLISTMETHOD)
+      }]
+    },
+    {
+      code: `
+        export default Ember.Component({
+          init() {
+            $()[${BLACKLISTMETHOD}]();
+          }
+        });`,
+      options: [BLACKLISTMETHOD],
+      errors: [{
+        message: getMessage(BLACKLISTMETHOD)
+      }]
+    },
+    {
+      code: `
+        import jQuery from 'jquery';
+        export default Ember.Component({
+          init() {
+            jQuery[${BLACKLISTMETHOD}]();
+          }
+        });`,
+      options: [BLACKLISTMETHOD],
+      errors: [{
+        message: getMessage(BLACKLISTMETHOD)
+      }]
+    },
+    {
+      code: `
+        import jQuery from 'jquery';
+        export default Ember.Component({
+          init() {
+            const _$ = jQuery;
+            _$[${BLACKLISTMETHOD}]();
+          }
+        });`,
+      options: [BLACKLISTMETHOD],
+      errors: [{
+        message: getMessage(BLACKLISTMETHOD)
+      }]
+    },
+    {
+      code: `
+        export default Ember.Component({
+          init() {
+            const myJQueryObj = this.$();
+            myJQueryObj[${BLACKLISTMETHOD}]();      
+          }
+        });`,
+      options: [BLACKLISTMETHOD],
+      errors: [{
+        message: getMessage(BLACKLISTMETHOD)
+      }]
+    },
+    {
+      code: `
+        import jQuery from 'jquery';
+        import fOO from 'foo';
+        export default Ember.Component({
+          init() {
+            jQuery[${BLACKLISTMETHOD}]();
+          }
+        });`,
+      options: [BLACKLISTMETHOD],
+      errors: [{
+        message: getMessage(BLACKLISTMETHOD)
+      }]
+    }
+  ]
+});
