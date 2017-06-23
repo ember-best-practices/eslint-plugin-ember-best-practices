d01f47d2c1298facd87a1f92bca5e24f1b9ba848
Steve Calvert <steve.calvert@gmail.com>
2017-06-23 15:52:32 -0700

    Adding extra support for Ember.run.debounce and Ember.run.throttle

--- guides/rules/require-ember-lifeline.md
+++ guides/rules/require-ember-lifeline.md
@@ -4,43 +4,6 @@
 
 Ember applications have long life-cycles. A user may navigate to several pages and use many different features before they leave the application. This makes JavaScript and Ember development unlike Rails development, where the lifecycle of a request is short and the environment disposed of after each request. It makes Ember development much more like iOS or video game development than traditional server-side web development.
 
-It is good to note that this isn't something inherent to just Ember. Any single-page app framework or solution (Angular, React, Vue, Backbone...) must deal this lifecycles of objects, and specifically with how async tasks can be bounded by a lifecycle.
+It is good to note that this isn't something inherent to just Ember. Any single-page app framework or solution (Angular, React, Vue, Backbone...) must deal with the lifecycles of objects, and specifically with how async tasks can be bounded by a lifecycle.
 
-Ember lifeline introduces several utility methods to help manage async, object lifecycles, and the Ember runloop. Of those utility methods, `runTask` can be used as a direct replacement for `Ember.run.later`, providing engineers a more robust, lifecyle aware variant for deferred operations.
-
-Example:
-
-```javascript
-import Ember from 'ember';
-
-const { Component, run } = Ember;
-
-export default Component.extend({
-  init() {
-    this._super();
-    run.later(() => {
-      // First, check if this object is even valid
-      if (this.isDestroying || this.isDestroyed) { return; }
-      this.set('date', new Date);
-    }, 500);
-  }
-});
-```
-
-becomes:
-
-```javascript
-import Ember from 'ember';
-import RunMixin from 'ember-lifeline/mixins/run';
-
-const { Component } = Ember;
-
-export default Component.extend(RunMixin, {
-  init() {
-    this._super();
-    this.runTask(() => {
-      this.set('date', new Date);
-    }, 500);
-  }
-});
-```
+Ember lifeline introduces several utility methods to help manage async, object lifecycles, and the Ember runloop. Please review the documentation to understand the APIs that can be used to replace the equivalent `Ember.run` method.
--- tests/lib/rules/require-ember-lifeline.js
+++ tests/lib/rules/require-ember-lifeline.js
@@ -1,5 +1,5 @@
 const rule = require('../../../lib/rules/require-ember-lifeline');
-const MESSAGE = rule.meta.message;
+const getMessage = rule.meta.message;
 const RuleTester = require('eslint').RuleTester;
 const ruleTester = new RuleTester();
 
@@ -7,6 +7,8 @@ ruleTester.run('require-ember-lifeline', rule, {
   valid: [
     {
       code: `
+        import Ember from 'ember';
+
         export default Ember.Component({
           init() {
             this.runTask(() => {
@@ -21,10 +23,80 @@ ruleTester.run('require-ember-lifeline', rule, {
     },
     {
       code: `
+        import Ember from 'ember';
+
         export default Ember.Component({
           actions: {
             foo() {
               this.runTask(() => {
+                doSomeWork();
+              });
+            }
+          }
+        });`,
+      parserOptions: {
+        ecmaVersion: 6,
+        sourceType: 'module'
+      }
+    },
+    {
+      code: `
+        import Ember from 'ember';
+
+        export default Ember.Component({
+          init() {
+            this.debounceTask(() => {
+              doSomeWork();
+            });
+          }
+        });`,
+      parserOptions: {
+        ecmaVersion: 6,
+        sourceType: 'module'
+      }
+    },
+    {
+      code: `
+        import Ember from 'ember';
+
+        export default Ember.Component({
+          actions: {
+            foo() {
+              this.debounceTask(() => {
+                doSomeWork();
+              });
+            }
+          }
+        });`,
+      parserOptions: {
+        ecmaVersion: 6,
+        sourceType: 'module'
+      }
+    },
+    {
+      code: `
+        import Ember from 'ember';
+
+        export default Ember.Component({
+          init() {
+            this.throttleTask(() => {
+              doSomeWork();
+            });
+          }
+        });`,
+      parserOptions: {
+        ecmaVersion: 6,
+        sourceType: 'module'
+      }
+    },
+    {
+      code: `
+        import Ember from 'ember';
+
+        export default Ember.Component({
+          actions: {
+            foo() {
+              this.throttleTask(() => {
                 doSomeWork();
               });
             }
@@ -39,6 +111,8 @@ ruleTester.run('require-ember-lifeline', rule, {
   invalid: [
     {
       code: `
+        import Ember from 'ember';
+
         export default Ember.Component({
           init() {
             Ember.run.later(() => {
@@ -51,11 +125,13 @@ ruleTester.run('require-ember-lifeline', rule, {
         sourceType: 'module'
       },
       errors: [{
-        message: MESSAGE
+        message: getMessage('Ember.run.later')
       }]
     },
     {
-      code: `
+      code: `        
+        import Ember from 'ember';
+
         export default Ember.Component({
           actions: {
             foo() {
@@ -70,11 +146,13 @@ ruleTester.run('require-ember-lifeline', rule, {
         sourceType: 'module'
       },
       errors: [{
-        message: MESSAGE
+        message: getMessage('Ember.run.later')
       }]
     },
     {
       code: `
+        import Ember from 'ember';
+
         export default Ember.Component({
           init() {
             run.later(() => {
@@ -87,11 +165,13 @@ ruleTester.run('require-ember-lifeline', rule, {
         sourceType: 'module'
       },
       errors: [{
-        message: MESSAGE
+        message: getMessage('run.later')
       }]
     },
     {
       code: `
+        import Ember from 'ember';
+
         export default Ember.Component({
           actions: {
             foo() {
@@ -106,7 +186,311 @@ ruleTester.run('require-ember-lifeline', rule, {
         sourceType: 'module'
       },
       errors: [{
-        message: MESSAGE
+        message: getMessage('run.later')
+      }]
+    },
+    {
+      code: `
+        import Ember from 'ember';
+
+        const {
+          run: foo
+        } = Ember;
+
+        export default Ember.Component({
+          init() {
+            foo.later(() => {
+              doSomeWork();
+            });
+          }
+        });`,
+      parserOptions: {
+        ecmaVersion: 6,
+        sourceType: 'module'
+      },
+      errors: [{
+        message: getMessage('foo.later')
+      }]
+    },
+    {
+      code: `
+        import Ember from 'ember';
+
+        const {
+          run: foo
+        } = Ember;
+
+        export default Ember.Component({
+          actions: {
+            bar() {
+              foo.later(() => {
+                doSomeWork();
+              });
+            }
+          }
+        });`,
+      parserOptions: {
+        ecmaVersion: 6,
+        sourceType: 'module'
+      },
+      errors: [{
+        message: getMessage('foo.later')
+      }]
+    },
+    {
+      code: `
+        import Ember from 'ember';
+
+        export default Ember.Component({
+          init() {
+            Ember.run.debounce(() => {
+              doSomeWork();
+            });
+          }
+        });`,
+      parserOptions: {
+        ecmaVersion: 6,
+        sourceType: 'module'
+      },
+      errors: [{
+        message: getMessage('Ember.run.debounce')
+      }]
+    },
+    {
+      code: `        
+        import Ember from 'ember';
+
+        export default Ember.Component({
+          actions: {
+            foo() {
+              Ember.run.debounce(() => {
+                doSomeWork();
+              });
+            }
+          }
+        });`,
+      parserOptions: {
+        ecmaVersion: 6,
+        sourceType: 'module'
+      },
+      errors: [{
+        message: getMessage('Ember.run.debounce')
+      }]
+    },
+    {
+      code: `
+        import Ember from 'ember';
+
+        export default Ember.Component({
+          init() {
+            run.debounce(() => {
+              doSomeWork();
+            });
+          }
+        });`,
+      parserOptions: {
+        ecmaVersion: 6,
+        sourceType: 'module'
+      },
+      errors: [{
+        message: getMessage('run.debounce')
+      }]
+    },
+    {
+      code: `
+        import Ember from 'ember';
+
+        export default Ember.Component({
+          actions: {
+            foo() {
+              run.debounce(() => {
+                doSomeWork();
+              });
+            }
+          }
+        });`,
+      parserOptions: {
+        ecmaVersion: 6,
+        sourceType: 'module'
+      },
+      errors: [{
+        message: getMessage('run.debounce')
+      }]
+    },
+    {
+      code: `
+        import Ember from 'ember';
+
+        const {
+          run: foo
+        } = Ember;
+
+        export default Ember.Component({
+          init() {
+            foo.debounce(() => {
+              doSomeWork();
+            });
+          }
+        });`,
+      parserOptions: {
+        ecmaVersion: 6,
+        sourceType: 'module'
+      },
+      errors: [{
+        message: getMessage('foo.debounce')
+      }]
+    },
+    {
+      code: `
+        import Ember from 'ember';
+
+        const {
+          run: foo
+        } = Ember;
+
+        export default Ember.Component({
+          actions: {
+            bar() {
+              foo.debounce(() => {
+                doSomeWork();
+              });
+            }
+          }
+        });`,
+      parserOptions: {
+        ecmaVersion: 6,
+        sourceType: 'module'
+      },
+      errors: [{
+        message: getMessage('foo.debounce')
+      }]
+    },
+    {
+      code: `
+        import Ember from 'ember';
+
+        export default Ember.Component({
+          init() {
+            Ember.run.throttle(() => {
+              doSomeWork();
+            });
+          }
+        });`,
+      parserOptions: {
+        ecmaVersion: 6,
+        sourceType: 'module'
+      },
+      errors: [{
+        message: getMessage('Ember.run.throttle')
+      }]
+    },
+    {
+      code: `        
+        import Ember from 'ember';
+
+        export default Ember.Component({
+          actions: {
+            foo() {
+              Ember.run.throttle(() => {
+                doSomeWork();
+              });
+            }
+          }
+        });`,
+      parserOptions: {
+        ecmaVersion: 6,
+        sourceType: 'module'
+      },
+      errors: [{
+        message: getMessage('Ember.run.throttle')
+      }]
+    },
+    {
+      code: `
+        import Ember from 'ember';
+
+        export default Ember.Component({
+          init() {
+            run.throttle(() => {
+              doSomeWork();
+            });
+          }
+        });`,
+      parserOptions: {
+        ecmaVersion: 6,
+        sourceType: 'module'
+      },
+      errors: [{
+        message: getMessage('run.throttle')
+      }]
+    },
+    {
+      code: `
+        import Ember from 'ember';
+
+        export default Ember.Component({
+          actions: {
+            foo() {
+              run.throttle(() => {
+                doSomeWork();
+              });
+            }
+          }
+        });`,
+      parserOptions: {
+        ecmaVersion: 6,
+        sourceType: 'module'
+      },
+      errors: [{
+        message: getMessage('run.throttle')
+      }]
+    },
+    {
+      code: `
+        import Ember from 'ember';
+
+        const {
+          run: foo
+        } = Ember;
+
+        export default Ember.Component({
+          init() {
+            foo.throttle(() => {
+              doSomeWork();
+            });
+          }
+        });`,
+      parserOptions: {
+        ecmaVersion: 6,
+        sourceType: 'module'
+      },
+      errors: [{
+        message: getMessage('foo.throttle')
+      }]
+    },
+    {
+      code: `
+        import Ember from 'ember';
+
+        const {
+          run: foo
+        } = Ember;
+
+        export default Ember.Component({
+          actions: {
+            bar() {
+              foo.throttle(() => {
+                doSomeWork();
+              });
+            }
+          }
+        });`,
+      parserOptions: {
+        ecmaVersion: 6,
+        sourceType: 'module'
+      },
+      errors: [{
+        message: getMessage('foo.throttle')
       }]
     }
   ]
